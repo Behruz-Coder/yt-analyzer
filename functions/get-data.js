f6b3ecf414366dbf8816375e91695feb0c6af903
Qodirberganov Behruz <uzb8972@gmail.com>
2026-04-02 22:56:59 +0500

    Enhance get-data.js with IP logging and API updates
    Updated YouTube API request URLs and added user IP logging.

--- functions/get-data.js
+++ functions/get-data.js
@@ -3,15 +3,19 @@ const axios = require('axios');
 exports.handler = async (event) => {
     const { type, q, channelId, idList } = event.queryStringParameters;
     const YT_KEY = process.env.YOUTUBE_API_KEY;
+    const userIP = event.headers['x-forwarded-for'] || 'unknown';
+
+    // IP Limit simulyatsiyasi (Aslida bazadan tekshiriladi)
+    console.log(`Request from IP: ${userIP}`);
 
     try {
         let url = "";
         if (type === "search") {
-            url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q)}&type=channel&maxResults=10&key=${YT_KEY}`;
+            url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q)}&type=channel&maxResults=15&key=${YT_KEY}`;
         } else if (type === "detail") {
-            url = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,brandingSettings&id=${channelId}&key=${YT_KEY}`;
-        } else if (type === "leaderboard") {
-            url = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${idList}&key=${YT_KEY}`;
+            url = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,brandingSettings,contentDetails&id=${channelId}&key=${YT_KEY}`;
+        } else if (type === "videos") {
+            url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=10&order=viewCount&type=video&key=${YT_KEY}`;
         }
 
         const res = await axios.get(url);
@@ -21,6 +25,6 @@ exports.handler = async (event) => {
             body: JSON.stringify(res.data)
         };
     } catch (e) {
-        return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+        return { statusCode: 500, body: JSON.stringify({ error: "API Error: " + e.message }) };
     }
 };
